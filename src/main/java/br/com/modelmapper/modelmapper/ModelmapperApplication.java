9d348f41dbad9eebc095284967698b8a9ecdc9e2
Leonardo <leonardo.sabino.s@hotmail.com>
2019-06-29 12:23:54 -0300

    Subindo base do projeto

---
+++ src/main/java/br/com/modelmapper/modelmapper/ModelmapperApplication.java
@@ -0,0 +1,13 @@
+package br.com.modelmapper.modelmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelmapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/modelmapper/modelmapper/ModelmapperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.modelmapper.modelmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelmapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
